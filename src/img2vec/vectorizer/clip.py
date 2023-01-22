f4b8ba0f71a54a93625064422e1aa2156832e45d
ahmadsalimi <ahsa9978@gmail.com>
2023-01-22 15:25:18 +0330

    Try to load only visual

--- src/img2vec/vectorizer/clip.py
+++ src/img2vec/vectorizer/clip.py
@@ -1,7 +1,11 @@
+import copy
+
 import clip
 import numpy as np
 import torch
 from PIL import Image
+from clip.model import CLIP
+from torchvision import transforms
 
 from img2vec.util import getLogger
 
@@ -18,12 +22,17 @@ class ClipVectorizer:
     def __init__(self, model: str, cuda: bool):
         self.__device = "cuda" if cuda and torch.cuda.is_available() else "cpu"
         logger.info(f'Using device: {self.__device}')
-        self.__model, self.__preprocess = clip.load(model, device=self.__device)
+        model, preprocess = clip.load(model, device=self.__device)
+        model: CLIP = model
+        self.__model = model.visual
+        self.__dtype = model.dtype
+        model.cpu()
+        self.__preprocess: transforms.Compose = preprocess
 
     @torch.cuda.amp.autocast()
     @torch.no_grad()
     @torch.inference_mode()
     def vectorize(self, image: Image.Image) -> np.ndarray:
-        image = self.__preprocess(image).unsqueeze(0).to(self.__device)
-        vector = self.__model.encode_image(image)
+        image: torch.Tensor = self.__preprocess(image).unsqueeze(0).to(self.__device)
+        vector = self.__model(image.type(self.__dtype))
         return vector.cpu().numpy().flatten()
